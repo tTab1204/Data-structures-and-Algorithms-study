186b305daa9a503420c5585ce33b1be1ef3d3fe0
LeeJuYoung <ken44929@gmail.com>
2021-02-08 22:26:34 +0900

    정렬(30), BFS(100), DFS(30) / 프로그래머스 문제풀이

---
+++ 알고리즘/BFS, DFS/BFS, DFS(특징).js
@@ -0,0 +1,22 @@
+// BFS(너비 우선 탐색) - 메모리가 더 많이 듬.
+// Shortest Path, Closer Nodes
+
+// DFS(깊이 우선 탐색) - 메모리가 더 적게 듬.
+
+//If you know a solution is not far from the root of the tree:
+// BFS
+
+//If the tree is very deep and solutions are rare,
+// BFS (DFS will take long time. )
+
+//If the tree is very wide: (too many nodes, it's not a binary tree)
+// DFS (BFS will need too much memory)
+
+//If solutions are frequent but located deep in the tree
+// DFS
+
+//determining whether a path exists between two nodes
+// DFS
+
+//Finding the shortest path
+// BFS
--- 알고리즘/정렬/02_05_정렬.js
+++ 알고리즘/정렬/02_05_정렬.js
@@ -3,6 +3,7 @@
 // 다른 문자 간의 비교는 sort((a,b) => a.localeCompare(b))를 통해 가능
 
 // 1. 버블정렬
+// 너무 느리다.
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function bubbleSort(array) {
@@ -25,7 +26,7 @@ bubbleSort(numbers);
 console.log(numbers);
 
 // 2. 선택정렬
-
+// 너무 느리다2.
 function selectionSort(array) {
   const length = array.length;
   for (let i = 0; i < length; i++) {
@@ -50,6 +51,7 @@ console.log(numbers);
 // 시간복잡도 높음 (=안 좋음)
 
 // 3. 삽입정렬
+// input이 적을 때, 요소들이 대부분 정렬되어 있을 때 매우 빠르다.
 function insertionSort(array) {
   const length = array.length;
   for (let i = 0; i < length; i++) {
@@ -76,3 +78,91 @@ function insertionSort(array) {
 
 insertionSort(numbers);
 console.log(numbers);
+
+// 4. 합병 정렬 (O(n log(n)))
+// 분할 및 정복, 매우 빠르다.
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function mergeSort(array) {
+  if (array.length === 1) {
+    return array;
+  }
+  // Split Array in into right and left
+  const length = array.length;
+  const middle = Math.floor(length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
+  // console.log('left:', left);
+  // console.log('right:', right);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  const result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  // console.log(left, right)
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+const answer = mergeSort(numbers);
+console.log(answer);
+
+// 바로 이해가 안된다2. 좀 더 보자.
+
+// 5. 퀵 정렬
+// 매우 빠르다. 공간 복잡도 매우 낮다.
+// worst case: 매우 느려질 위험이 있다.
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function quickSort(array, left, right) {
+  const len = array.length;
+  let pivot;
+  let partitionIndex;
+
+  if (left < right) {
+    pivot = right;
+    partitionIndex = partition(array, pivot, left, right);
+
+    //sort left and right
+    quickSort(array, left, partitionIndex - 1);
+    quickSort(array, partitionIndex + 1, right);
+  }
+  return array;
+}
+
+function partition(array, pivot, left, right) {
+  let pivotValue = array[pivot];
+  let partitionIndex = left;
+
+  for (let i = left; i < right; i++) {
+    if (array[i] < pivotValue) {
+      swap(array, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  swap(array, right, partitionIndex);
+  return partitionIndex;
+}
+
+function swap(array, firstIndex, secondIndex) {
+  var temp = array[firstIndex];
+  array[firstIndex] = array[secondIndex];
+  array[secondIndex] = temp;
+}
+
+//Select first and last index as 2nd and 3rd parameters
+quickSort(numbers, 0, numbers.length - 1);
+console.log(numbers);
+
+// 바로 이해가 안된다3. 좀 더 보자.

fc8d4c66295e1a317431d9bac5833a490469cee8
LeeJuYoung <ken44929@gmail.com>
2021-03-17 10:06:04 +0900

    프로그래머스 문제풀이

---
+++ 프로그래머스 문제 풀이/Level 2/03_16.문제풀이.js
@@ -0,0 +1,142 @@
+// 1. 최댓값과 최솟값
+function solution(s) {
+  let arr = [];
+  let temp = [];
+  arr = s.split(" ").sort((a, b) => a - b);
+
+  let min = arr.shift();
+  let max = arr.pop();
+
+  temp.push(min);
+  temp.push(max);
+
+  return temp.join(" ");
+}
+
+// 다른 사람의 풀이
+function solution(s) {
+  const arr = s.split(" ");
+
+  return Math.min(...arr) + " " + Math.max(...arr);
+}
+
+// Math.max, Math.min을 생각 못했다. 여러 내장 함수를 잘 활용해보자.
+// 안에 전개 연산자로 넣으니까 되네
+// max, min이 문자열도 되는게 신기 (Number, String이렇게 써줄 필요 없음)
+
+// 2. 숫자의 표현
+// 혼자 못 풀었다.
+// 투포인트 알고리즘
+
+function solution(n) {
+  let answerNum = 0;
+  let middle = Math.ceil(n / 2); // middle까지만 확인하면 됨
+  let sum = 0;
+  let arr = [];
+
+  // 더하기 위해서 배열 생성
+  for (let i = 1; i <= middle; i++) {
+    arr.push(i);
+  }
+
+  // 배열의 처음요소부터 순차적으로 더하다가 합이 n이면 다음 숫자부터 다시 반복
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      sum = sum + arr[j];
+      if (sum === n) {
+        answerNum++;
+        break;
+      } else if (sum > n) {
+        break;
+      }
+    }
+    sum = 0;
+  }
+
+  return answerNum + 1; //자기자신(n)일 경우 고려
+}
+
+// 내껀 시간초과되는데 왜 이건 시간초과가 안되는걸까
+// 생각해보고 다시 풀기 ㄱㄱ
+
+// 3. 가장 큰 정사각형 찾기
+
+// 혼자 못 풀었다. 나중에 다시 풀어보기.
+
+// 다이나믹 프로그래밍(Dynamic Programming)으로 푸는 문제
+function solution(board) {
+  var x_len = board[0].length;
+  var y_len = board.length;
+  var answer = 0;
+
+  // 배열의 행과 열이 1이면 정사각형은 무조건 1이 나온다. // 0이 나올수도 있긴함.
+  if (x_len < 2 || y_len < 2) return 1;
+
+  // 1부터 쭉 탐색하면서 자신의 왼쪽 왼쪽상단, 상단 3개의 값을 비교하며 값을 채워넣는다.
+  for (var i = 1; i < y_len; i++) {
+    for (var j = 1; j < x_len; j++) {
+      if (board[i][j] > 0) {
+        let min = Math.min(
+          board[i - 1][j - 1], // 0
+          board[i][j - 1], // 1 (세로: 두번째 배열의 0번지 요소)
+          board[i - 1][j] // 1 (가로: 첫번째 배열의 1번지 요소)
+        );
+        board[i][j] = min + 1;
+      }
+      if (answer < board[i][j]) {
+        answer = board[i][j];
+      }
+    }
+  }
+  return answer * answer;
+}
+board = [
+  [0, 1, 1, 1],
+  [1, 1, 1, 1],
+  [1, 1, 1, 1],
+  [0, 0, 1, 0],
+];
+
+//모르겠당 모르겠당 모르겠당
+// ???????????????
+// 나중에 다시 ㄱㄱ (동적 프로그래밍 이해 더 잘하고 다시 ㄱㄱㄱㄱ)
+
+// 4. 소수찾기
+const solution = (numbers) => {
+  let max = numbers
+    .split("")
+    .sort((a, b) => b - a)
+    .join("");
+  let maxNum = Number(max);
+  for (let i = 2; i <= maxNum; i++) {}
+};
+
+numbers = "17";
+solution(numbers);
+
+//????????????????????
+// 나중에 다시 이것도 다시
+
+// 5. 카펫
+function solution(brown, yellow) {
+  let sum = brown + yellow;
+  let n = 3;
+  let temp = [];
+
+  if (sum % 2 === 1) {
+    // 조건 2 : 짝수는 x+y의 약수를 나열 했을 때, (x+y)/2 바로 앞에 있는 두 수가 된다.
+    for (let i = 0; i < sum / 2; i++) {
+      if (sum % i === 0) temp.push(i);
+    }
+    return [temp[temp.length - 1], temp[temp.length - 2]];
+  }
+  // 홀수일 때는 제곱했을 때 밖에 없음.
+  else {
+    while (n * n < sum) {
+      if (n * n === sum) {
+        return n;
+      }
+      n++;
+    }
+  }
+}
--- 프로그래머스 문제 풀이/연습장.js
+++ 프로그래머스 문제 풀이/연습장.js
@@ -4,3 +4,31 @@ function solution(s) {
   }
   return false;
 }
+
+// 숫자의 표현
+function solution(n) {
+  let start = 0;
+  let end = 0;
+  let arr = [];
+  let answerNum = 0;
+  let sum = 0;
+
+  for (let i = 1; i <= n; i++) {
+    arr.push(i);
+  }
+
+  while (arr[end] <= 15) {
+    for (let i = start; i <= end; i++) {
+      sum += arr[i];
+    }
+    if (sum < n) end++;
+    else if (sum > n) start++;
+    else {
+      answerNum++;
+      end++;
+      sum = 0;
+    }
+  }
+  return answerNum;
+}
+// 왜 시간초과지 왜 시간초과지 왜 시간초과인데

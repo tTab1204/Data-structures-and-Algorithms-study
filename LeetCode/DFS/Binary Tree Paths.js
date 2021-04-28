e410062ff23a15f42b6ae141b6368711eeffccf8
LeeJuYoung <ken44929@gmail.com>
2021-04-28 21:57:01 +0900

    LeetCode 문제풀이

--- LeetCode/DFS/Binary Tree Paths.js
+++ LeetCode/DFS/Binary Tree Paths.js
@@ -1,4 +1,19 @@
-var binaryTreePaths = function (root) {};
+var binaryTreePaths = function (root) {
+  // speical case
+  if (!root) return [];
 
-// 재귀 이용
-// 내일 다시 해보자.
+  var result = [];
+
+  function path(root, str) {
+    if (!root.left && !root.right) result.push(str + root.val);
+
+    if (root.left) path(root.left, str + root.val + "->");
+
+    if (root.right) path(root.right, str + root.val + "->");
+  }
+  path(root, "");
+  return result;
+};
+
+// 아직 이해 덜 됨
+// 재귀의 동작 방식 이해하고 다시 풀기
